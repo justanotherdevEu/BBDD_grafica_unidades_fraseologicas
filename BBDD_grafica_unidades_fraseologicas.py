249ba139d2fc579c5da4b16f592f11643709c94c
justanotherdevEu <75378683+justanotherdevEu@users.noreply.github.com>
2024-06-12 03:15:15 +0200

    Update BBDD_grafica_unidades_fraseologicas.py

--- BBDD_grafica_unidades_fraseologicas.py
+++ BBDD_grafica_unidades_fraseologicas.py
@@ -1,67 +1,16 @@
 #   se supone que esto debe ejecutarse en Windows
 import os
 import json
-"""import boto3
-from botocore.exceptions import ClientError"""
-#from boto3.dynamodb import Table
-#  ARN del rol   DynamoDBRole_full_ADMIN   : arn:aws:iam::590183836735:role/DynamoDBRole_full_ADMIN
+
 #instalamos de nuevo Python, por ejemplo Python 3.10      por si acaso, con winget por ejemplo, un gestor de paquetes ya integrado en Windows
 print("\n\t\t Comprobando que Python 3.10 está instalado.....")
 os.system('winget install "Python.Python.3.10"')
 #print("\n\t\t Comprobando que librerías boto3 y AWS están instaladas.....")
 
-#instalamos por si no lo estuviera, AWS y boto3
-"""os.system("pip install AWS")            #   Instala y configura AWS CLI con tus credenciales de AWS, aunque las credenciales irán aparte
-os.system("pip install boto3")          #   Instala Boto3, el SDK de Python para AWS, con el comando pip install boto3
-
-# Establece la región de DynamoDB
-region = "us-east-1"
-
-# Crea una sesión en la región deseada
-session = boto3.Session(region_name=region)
-
-# Crea un cliente de STS
-sts_client = session.client('sts')"""
-
-
-#boto3.set_stream_logger('botocore', level='DEBUG')
-
-# Asume el rol y obtén las credenciales temporales
-"""assumed_role = sts_client.assume_role(
-    RoleArn="arn:aws:iam::xxxxxxxxxxxxxxxxxx:role/xxxxxxxxxxxxxxxx",
-    RoleSessionName="AssumeRoleSession1"
-)"""
-"""assumed_role = sts_client.assume_role(
-    RoleArn="arn:aws:iam::xxxxxxxxxx:user/xxxxxxxxxxxxxxx",
-    RoleSessionName="AssumeRoleSession1"
-)"""
-"""#credentials = assumed_role['Credentials']
-credentials = {}
-
-# Crea un cliente de DynamoDB usando las credenciales temporales
-clave = input("Introduce clave de acceso:\t")
-clave_sec = input("Introduce clave de acceso secreta:\t")
-credentials['AccessKeyId'] = clave
-credentials['SecretAccessKey'] = clave_sec
-dynamodb = boto3.resource(      'dynamodb', 
-                                aws_access_key_id=credentials['AccessKeyId'],
-                                aws_secret_access_key=credentials['SecretAccessKey'],
-                                #aws_session_token=credentials['SessionToken'],
-                                region_name=region,
-                                endpoint_url='https://dynamodb.us-east-1.amazonaws.com')"""
-
-#aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
-#aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
-#aws_session_token=assumed_role['Credentials']['SessionToken'],
-
-# Nombre de la tabla de DynamoDB
-nombre_tabla = "unidades_fraseologicas"
 
 
 # Inicializamos el diccionario vacío
 db = {}
-#db_aws = {}
-
 
 # Abre el archivo existente y lee el diccionario, para que el diccionario se meta en memoria, y luego se le añada en memoria las adiciones, y luego se meta el diccionario nuevo con adicciones ( o sin ellas, igual solo hubo consultas)
 
@@ -208,97 +157,11 @@ while True:
 ruta = os.getcwd()
 ruta_completa = os.path.join(ruta, filename)
 
-# Compara los diccionarios y sincroniza los datos para que lo que no hay en 'db' pero sí en 'db_aws', esté en ambos
-"""def aws_hacia_db():
-    for clave in db_aws.keys():
-        if clave not in db:
-            db[clave] = db_aws[clave]
-        elif db[clave] != db_aws[clave]:
-            db[clave] = db_aws[clave]
 
-# Compara los diccionarios y sincroniza los datos para que lo que no hay en 'db_aws' pero sí en 'db', esté en ambos
-def db_hacia_aws():
-    for clave in db.keys():
-        if clave not in db_aws:
-            db_aws[clave] = db[clave]
-        elif db_aws[clave] != db[clave]:
-            db_aws[clave] = db[clave]"""
-
-#sync = input("""\n\n\t\tAhora tiene que elegir entre:\n\t1. \tSincronizar los datos leídos de la tabla sincronizada en AWS, hacia la tabla en local
-#                    \n\t2. \tSincronizar los datos en local que no están en la nube y subirlos a la nube\n\t3. \tSincronizar ambos con lo que no tiene del otro\n\t4. Ignorar y seguir""")
-"""while sync not in list(range(1,5)):     
-        try:
-            sync = int(input("Elige una opción (1-4): "))
-        except ValueError:
-            print("Por favor, introduce un número válido entre 1 y 4.")
-            sync = None
-sync = int(sync)
-if sync == 1:
-    aws_hacia_db()
-    print("\n\t\t\t\tSincronización de AWS a los archivos locales correcta")
-elif sync == 2:
-    db_hacia_aws()
-    print("\n\t\t\t\tSincronización de archivos locales a AWS correcta")
-elif sync == 3:
-    aws_hacia_db()
-    db_hacia_aws()
-    print("\n\t\t\t\tSincronización bidireccional entre local y AWS correcta")
-elif sync == 4:
-    pass
-else:
-    sync = None
-    print("\n\n\tHa ocurrido un error, la entrada de usuario para elegir sincronización tiene un valor no permitido\n\t\t\tRevisar 0x000000059619583c")"""
-#sync = None
 os.system("pause"), os.system("cls")  # pausa y borrar lo impreso hasta el momento en consola para limpiar
 
 
 
-'''#                                   ahora preguntar al usuario si quiere borrar de AWS lo que no esté en local
-sync = input("\n\n\t\t\t¿Desea borrar en local todas las entradas de la tabla sincronizada en AWS que NO estén en local?\n\t\tY / N:\t")
-while sync not in ('Y', 'N'):
-    sync = input("\nY / N:\t")
-
-if sync == 'Y':
-# Compara las claves de db y db_aws en orden inverso para detectar claves que ya no existen en 'db_aws' , y borrarlas también de 'db'
-    for clave in reversed(list(db.keys())):
-        if clave not in db_aws:
-            del db[clave]
-elif sync == 'N':
-    pass
-else:
-    sync = None
-    os.system("pause")
-    print("\n\n\tHa ocurrido un error, la entrada de usuario para elegir sincronización tiene un valor no permitido\n\t\t\tRevisar 0x000000059619583c")'''
-
-
-
-"""table = dynamodb.Table(nombre_tabla)
-
-# Actualiza la tabla de DynamoDB con los datos de db_aws
-for clave, valor in db_aws.items():
-    # Crea un objeto de actualización de elementos
-    actualizacion = {
-        'Key': {'clave': clave},
-        'UpdateExpression': 'SET valor = :v',
-        'ExpressionAttributeValues': {
-            ':v': valor
-        },
-        'ReturnValues': 'UPDATED_NEW'
-    }
-
-# Establece los valores de clave y valor para la actualización
-#actualizacion['ExpressionAttributeValues'][':k'] = clave
-
-    # Intenta actualizar el elemento existente
-    try:
-        table.update_item(**actualizacion)
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
-            # La clave no existe, crea un nuevo elemento
-            # La clave no existe, crea un nuevo elemento
-            table.put_item(Item={'clave': clave, 'valor': valor})
-        else:
-            raise e"""
 
 #       ahora a sobreescribir en local con 'db'
 try:
@@ -316,22 +179,13 @@ while pregunta == None or pregunta not in ("Y", "N"):
 
 if pregunta == "Y":
     with open(ruta_completa,'r', encoding='utf-8') as f:
-        """print(f"\n\t\tcontenido de  {filename} ")
-        cont = f.read()
-        print("\n\n\t\tcontenido:\t"+str(cont))
-        f.close()
-        del cont"""
         listar_db()
-        """print("keys:\t "+str(db.keys()))         
-        print("\tvalues:\t "+str(db.values()))
-        del db"""                               # esto fue para probar a ver todas las claves y valores por separado
         os.system("pause")
         os.system("cls")
-        "del os, pregunta"
         print("\t\t\t\t\t\t\tMuchas gracias, ¡Hasta pronto!")
 else:
     os.system("cls")
     del os, pregunta, db
     print("\n\n")
     print("\t\t\t\t\t\t\tMuchas gracias, ¡Hasta pronto!")
-
+    
